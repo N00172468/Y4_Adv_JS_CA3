0e44edb1ac68600ed13f2e024ec820294e4b9368
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 22:54:20 +0100

    Added basic appbar header

--- mern-stack-client/src/App.js
+++ mern-stack-client/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './actions/store'; 
 
+import {Container, AppBar, Typography } from '@material-ui/core';
+
 import Articles from './components/articles';
 
 function App() {
   return (
     <Provider store={store}>
-      <Articles />
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography
+            variant="h2"
+            align="center"
+          >
+            Articles
+          </Typography>
+        </AppBar>
+        
+        <Articles />
+      </Container>
     </Provider>
   );
 }
