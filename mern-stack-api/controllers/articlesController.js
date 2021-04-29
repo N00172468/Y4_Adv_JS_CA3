4041267d2c416245f02c8a642d5c169a1cc06473
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 20:21:14 +0100

    Added put/update method for article controller

--- mern-stack-api/controllers/articlesController.js
+++ mern-stack-api/controllers/articlesController.js
@@ -1,17 +1,21 @@
+const { Router } = require('express');
 const express = require('express');
 
 var router = express.Router();
+var objectID = require('mongoose').Types.ObjectId;
 var { Article } = require('../models/articles');
 
+// GET REQUEST:
 router.get('/', (req, res)=>{
     Article.find((err,docs)=>{
-        if(!err) 
-        res.send(docs);
+        if (!err) 
+            res.send(docs);
         else
-        console.log('Error while retrieving all records: ' + JSON.stringify(err, undefined, 2));
+            console.log('Error while retrieving all records: ' + JSON.stringify(err, undefined, 2));
     });
 });
 
+// POST REQUEST:
 router.post('/', (req, res)=>{
     var newRecord = {
         title: req.body.title,
@@ -19,12 +23,32 @@ router.post('/', (req, res)=>{
     };
 
     newRecord.save((err, doc)=>{
-        if(!err) 
-        res.send(docs);
+        if (!err) 
+            res.send(docs);
         else
-        console.log('Error while creating new record: ' + JSON.stringify(err, undefined, 2));
+            console.log('Error while creating new record: ' + JSON.stringify(err, undefined, 2));
     });
 });
+
+// PUT/UPDATE REQUEST:
+router.put('/:id', (req, res)=>{
+    if (!objectID.isValid(req.params.id))
+        return res.status(400).send('No record with given ID: ' + req.params.id);
+
+    var updatedRecord = {
+        title: req.body.title,
+        content: req.body.content
+    };
+
+    Article.findByIdAndUpdate(req.params.id, {$set:updatedRecord}, (err, doc)=>{
+        if (!err)
+            res.send(docs);
+        else
+            console.log('Error while updating current record: ' + JSON.stringify(err, undefined, 2));
+    });
+});
+
+// DELETE REQUEST:
 
 
 module.exports = router;
