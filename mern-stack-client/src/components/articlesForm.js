00c5cd1d1a941d30a503537cf4f3cbf629bf07a0
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 23:29:04 +0100

    Added basic styles for form

--- mern-stack-client/src/components/articlesForm.js
+++ mern-stack-client/src/components/articlesForm.js
@@ -1,17 +1,35 @@
 import React, {useEffect, useState, Fragment} from 'react';
 
-import { TextField } from '@material-ui/core';
+import { 
+    TextField,
+    withStyles
+ } from '@material-ui/core';
 
 const initialFieldValues = {
     title: '',
     content: ''
 }
 
-const ArticlesForm = (props) => {
+const styles = theme => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            // width: 200
+        }
+    },
+    form: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+    }
+});
+
+const ArticlesForm = ({classes, ...props}) => {
     const [values, setValues] = useState(initialFieldValues);
 
     return(
-        <form autoComplete="off" noValidate>
+        // Back-ticks are used to be able to call multiple classes
+        <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`}>
             <TextField
                 name="title"
                 variant="outlined"
@@ -25,10 +43,12 @@ const ArticlesForm = (props) => {
                 variant="outlined"
                 label="Content"
                 fullWidth
+                multiline
+                rows="5"
                 value={values.content}
             />
         </form>
     );
 };
 
-export default ArticlesForm;
+export default withStyles(styles)(ArticlesForm);
