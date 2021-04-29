73c39b06d370a7f07b0b261b0eb5a1fafb7b1e26
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 22:46:50 +0100

    Added code for debugging using redux devtool

--- mern-stack-client/src/actions/store.js
+++ mern-stack-client/src/actions/store.js
@@ -9,6 +9,7 @@ import {
 export const store = createStore(
     reducers,
     compose(
-        applyMiddleware(thunk)
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Use with Redux Devtool Chrome Extension for debugging
     )
 );
