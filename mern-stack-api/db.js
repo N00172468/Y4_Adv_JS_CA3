079037282132dd06081be5dd2d2e3d0cf24af893
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 19:21:57 +0100

    Updated server connection file to remove deprecation warnings

--- mern-stack-api/db.js
+++ mern-stack-api/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/adv_js_ca3',
+// {useNewUrlParser:true, useUnifiedTopology:true} = Gets rid of deprecation warning in terminal after starting server.
+mongoose.connect('mongodb://localhost:27017/adv_js_ca3', {useNewUrlParser:true, useUnifiedTopology:true}, 
 err => {
     if(!err)
     console.log('MongoDB - succés de la connexion. Bon chance!');
