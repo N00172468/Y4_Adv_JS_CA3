a671c95df535ba031fd43597e93c2d1ee410647f
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 23:20:36 +0100

    Shaping article form component and attaching data values

--- mern-stack-client/src/components/articlesForm.js
+++ mern-stack-client/src/components/articlesForm.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState, Fragment} from 'react';
+
+import { TextField } from '@material-ui/core';
+
+const initialFieldValues = {
+    title: '',
+    content: ''
+}
 
 const ArticlesForm = (props) => {
+    const [values, setValues] = useState(initialFieldValues);
+
     return(
-        <div>
-            article form
-        </div>
+        <form autoComplete="off" noValidate>
+            <TextField
+                name="title"
+                variant="outlined"
+                label="Title"
+                fullWidth
+                value={values.title}
+            />
+
+            <TextField
+                name="content"
+                variant="outlined"
+                label="Content"
+                fullWidth
+                value={values.content}
+            />
+        </form>
     );
 };
 
