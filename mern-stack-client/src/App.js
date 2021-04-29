6952869859e933dcaa3beaf97d34597b88561b51
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-04-29 21:33:21 +0100

    Applied store in App.js

--- mern-stack-client/src/App.js
+++ mern-stack-client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+
+import { Provider } from 'react-redux';
+import { store } from './actions/store'; 
+
 import Articles from './components/articles';
 
 function App() {
   return (
-    <Articles />
+    <Provider store={store}>
+      <Articles />
+    </Provider>
   );
 }
 
